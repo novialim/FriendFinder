0cd854603310c01e2e0ca363c187508c300c3b8d
novialim <novialim@gmail.com>
2017-07-15 23:03:41 -0700

    Basic features completed

---
+++ app/data/friends.js
@@ -0,0 +1,97 @@
+// Celebrities (DATA)
+// =============================================================
+var celebrities = [
+	{
+		"name": "Ryan Reynolds",
+		"photo": "http://pinthisstar.com/images/ryan-reynolds-images-14.jpg",
+		"scores": [ 
+			"5",
+			"1",
+			"4",
+			"4",
+			"5",
+			"1",
+			"2",
+			"5",
+			"4",
+			"5"
+		]
+	}, {
+		"name": "Channing Tatum",
+		"photo": "http://media.gq.com/photos/5582c641e52bc4b477a9d12b/master/w_704,h_964,c_limit/style-2011-03-channing-tatum-channing-tatum-outtake06.jpg",
+		"scores": [
+			"4",
+			"2",
+			"5",
+			"1",
+			"3",
+			"2",
+			"2",
+			"1",
+			"3",
+			"2"
+		]
+	}, {
+		"name": "Gerard Butler",
+		"photo": "https://s-media-cache-ak0.pinimg.com/736x/f7/99/15/f7991526343668d1468dcc0ab0ebec22--phantom-of-the-opera-beautiful-men.jpg",
+		"scores": [
+			"5",
+			"2",
+			"2",
+			"2",
+			"4",
+			"1",
+			"3",
+			"2",
+			"5",
+			"5"
+		]
+	}, {
+		"name": "Hugh Jackman",
+		"photo": "https://1.soompi.io/wp-content/uploads/2015/10/hugh-jackman.jpg",
+		"scores": [
+			"3",
+			"3",
+			"4",
+			"2",
+			"2",
+			"1",
+			"4",
+			"2",
+			"2",
+			"3"
+		]
+	}, {
+	"name": "Bradley Cooper",
+		"photo": "https://www.berlinale.de/media/bilder/2014/starportraits2014/2014-02-07-9679-1069_Bradley_Cooper_IMG_x900.jpg",
+		"scores": [
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4",
+			"4"
+		]
+	}, {
+		"name": "Christian Bale",
+		"photo": "http://msnbcmedia.msn.com/i/MSNBC/Components/Photo/_new/120909-christian-bale.jpg",
+		"scores": [
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5"
+		]
+	}
+];
+
+module.exports = celebrities;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,44 @@
 // Your apiRoutes.js file should contain two routes:
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+'use strict';
+
+var celebrityData = require ('../data/friends');
+
+module.exports = function (app) {
+
+	app.get('/api/friends', function (req, res){
+		res.json(celebrityData);
+	});
+
+	app.post('/api/friends', function (req, res){
+
+		var smallestScore = 0;
+		var bestMatch;
+
+		for (var i=0; i<celebrityData.length; i++){
+			var compArray = [];
+
+			for (var j=0; j< celebrityData[i].scores.length; j++){
+				compArray.push(Math.abs(celebrityData[i].scores[j] - req.body.scores[j]));
+			}
+
+			var matchScore = compArray.reduce((a,b) => a+b, 0);
+
+			if(smallestScore == 0){
+				smallestScore = matchScore;
+			}
+
+			if (matchScore < smallestScore){
+				smallestScore = matchScore;
+
+				bestMatch = celebrityData[i];
+			}
+		}
+
+		res.json(bestMatch);
+
+		celebrityData.push(req.body);
+	});
+}
